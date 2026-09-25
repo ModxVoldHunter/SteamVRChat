c97184d0271d8dde768c2630eaaa64a47c3c8f8c
ModxVoldHunter <65139923+ModxVoldHunter@users.noreply.github.com>
2026-09-24 21:22:15 -0400

    Pending changes exported from your codespace (#14)

--- Program.cs
+++ Program.cs
@@ -6,28 +6,39 @@ namespace SteamVRChatRun
     {
         public static class Setup
         {
-
-            public static string SVR = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\SteamVR\\bin\\win64\\vrdashboard.exe";
-            public static string VRC = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\VRChat\\start_protected_game.exe";
+            public static string VRC, SVR;
+            public static string[] GamesList = {
+                    VRC = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\VRChat\\start_protected_game.exe",
+                    SVR = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\SteamVR\\bin\\win64\\vrdashboard.exe"
+            };
             public class Games
             {
+
+                public static void Start(string[] GameList, GameType type)
+                {
+                    Process Process = new Process();
+                    Process.StartInfo.FileName = GameList[(int)type];
+                }
+
+                public enum GameType : int
+                {
+                    VRC = 1,
+                    SVR = 2
+                }
                 public static bool IsOpen = false;
-                public static int clock = 30;
-                public static Process Process = new Process();
+                public static int clock = 15;
                 public static void RunClient()
                 {
+                        
                     if (IsOpen)
                     {
-                        
-                        Process.StartInfo.FileName = SVR;
-                        Process.Start();
+                        Start(GamesList, GameType.SVR);
                         while (clock > 0)
                         {
                             Console.WriteLine(clock--);
                             Thread.Sleep(1000); // Wait for 1 second
                         }
-                        Process.StartInfo.FileName = VRC;
-                        Process.Start();
+                        Start(GamesList, GameType.VRC);
                         while (clock > 0)
                         {
                             Console.WriteLine(clock--);
